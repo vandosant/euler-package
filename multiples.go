546f0c048bbe25387ee9e51bb5565557a420acbf
vandosant <sjskender@gmail.com>
2014-08-01 14:43:09 -0600

    Calculates the sum of all multiples below 1000

--- multiples.go
+++ multiples.go
@@ -1,7 +1,14 @@
 package euler
 
 func Multiples() int {
-	var x int
-	x = 1
+	x := 0
+	for i := 0; i < 1000; i++ {
+		if i % 3 == 0 {
+			x += i
+		} else if i % 5 == 0 {
+			x += i
+		}
+	}
+
 	return x
 }
