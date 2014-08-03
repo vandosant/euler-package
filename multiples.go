a52af6afde88991c2c4eaedd74649ebff07d8b41
vandosant <sjskender@gmail.com>
2014-08-02 18:20:46 -0600

    Added Fibonacci and SumEvenValues functions

--- multiples.go
+++ multiples.go
@@ -3,12 +3,41 @@ package euler
 func SumMultiples(v int) int {
 	x := 0
 	for i := 0; i < v; i++ {
-		if i % 3 == 0 {
+		if i%3 == 0 {
 			x += i
-		} else if i % 5 == 0 {
+		} else if i%5 == 0 {
 			x += i
 		}
 	}
 
 	return x
+}
+
+func Fibonacci(v int) []int {
+	x := 1
+	y := 2
+	r := []int{x, y}
+
+	for i := 2; i < v; i++ {
+		x = r[i-1]
+		y = r[i-2]
+		n := x + y
+		r = append(r, n)
+		if n > v {
+			break
+		}
+	}
+
+	return r
+}
+
+func SumEvenValues(s []int) int {
+	r := 0
+	for i := range s {
+		if s[i]%2 == 0 {
+			r += s[i]
+		}
+	}
+
+	return r
 }
